4233a46b4817c93ea7e01c74cbefd474f4cf4390
Kevin Kipkemboi <theptjhub@gmail.com>
2023-08-22 19:33:17 -0700

    fixed comment feed issue

--- components/posts/CommentFeed.tsx
+++ components/posts/CommentFeed.tsx
@@ -5,25 +5,25 @@ import { formatDistanceToNowStrict } from 'date-fns';
 import Avatar from '../Avatar';
 
 interface CommentItemProps {
-  data: Record<string, any>;
+  comments: Record<string, any>;
 }
 
-const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
+const CommentItem: React.FC<CommentItemProps> = ({ comments = {} }) => {
   const router = useRouter();
 
   const goToUser = useCallback((ev: any) => {
     ev.stopPropagation();
 
-    router.push(`/users/${data.user.id}`)
-  }, [router, data.user.id]);
+    router.push(`/users/${comments.user.id}`)
+  }, [router, comments.user.id]);
 
   const createdAt = useMemo(() => {
-    if (!data?.createdAt) {
+    if (!comments?.createdAt) {
       return null;
     }
 
-    return formatDistanceToNowStrict(new Date(data.createdAt));
-  }, [data.createdAt])
+    return formatDistanceToNowStrict(new Date(comments.createdAt));
+  }, [comments.createdAt])
 
   return (
     <div 
@@ -36,7 +36,7 @@ const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
         transition
       ">
       <div className="flex flex-row items-start gap-3">
-        <Avatar userId={data.user.id} />
+        <Avatar userId={comments.user.id} />
         <div>
           <div className="flex flex-row items-center gap-2">
             <p 
@@ -47,7 +47,7 @@ const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
                 cursor-pointer 
                 hover:underline
             ">
-              {data.user.name}
+              {comments.user.name}
             </p>
             <span 
               onClick={goToUser} 
@@ -58,14 +58,14 @@ const CommentItem: React.FC<CommentItemProps> = ({ data = {} }) => {
                 hidden
                 md:block
             ">
-              @{data.user.username}
+              @{comments.user.username}
             </span>
             <span className="text-neutral-500 text-sm">
               {createdAt}
             </span>
           </div>
           <div className="text-white mt-1">
-            {data.body}
+            {comments.body}
           </div>
         </div>
       </div>
